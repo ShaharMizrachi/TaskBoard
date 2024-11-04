baf11adb56e596648f356936325c3402245167dd
Shahar <shaharm22@gmail.com>
2024-11-04 13:18:58 +0200

    validation middleware for task creation and updates

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createTask, getTask, updateTask } = require("../controllers/taskController");
+const { createTask, getTask, updateTask, getTasks } = require("../controllers/taskController");
+const { validateTask } = require("../middleware/validators");
 
-router.post("/tasks", createTask);
+router.post("/tasks", validateTask, createTask);
+router.put("/tasks/:id", validateTask, updateTask);
+router.get("/tasks", getTasks);
 router.get("/tasks/:id", getTask);
-router.put("/tasks/:id", updateTask);
 
 module.exports = router;
